59905f867f1aed35bbd41be20d15fe9fe7f75a98
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-11-11 12:15:08 +0100

    Projet eclipse WordCount

---
+++ src/main/scala/fr/blaise/spark/WordCount.scala
@@ -0,0 +1,23 @@
+package fr.blaise.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("WordCount")
+    val sc = new SparkContext(conf)
+
+    val begin = System.currentTimeMillis();
+    val textFile = sc.textFile("big.txt")
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .sortBy(-_._2)
+      .take(10);
+    counts.foreach(println)
+    
+    val end = System.currentTimeMillis();
+  }
+  
+}
